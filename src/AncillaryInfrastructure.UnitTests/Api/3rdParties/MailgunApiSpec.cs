759e7014cbd1dd5b1784b2dc4a297271ee312188
Jezz Santos <jezz.santos@hotmail.com>
2024-08-03 11:00:28 +1200

    Improvements to Mailgun implementation

--- src/AncillaryInfrastructure.UnitTests/Api/3rdParties/MailgunApiSpec.cs
+++ src/AncillaryInfrastructure.UnitTests/Api/3rdParties/MailgunApiSpec.cs
@@ -1,14 +1,15 @@
 using AncillaryApplication;
 using AncillaryInfrastructure.Api._3rdParties;
 using Application.Interfaces;
+using Application.Resources.Shared;
 using Common;
 using Common.Configuration;
-using Common.Extensions;
 using FluentAssertions;
 using Infrastructure.Interfaces;
 using Infrastructure.Shared.ApplicationServices.External;
 using Infrastructure.Web.Api.Interfaces;
 using Infrastructure.Web.Api.Operations.Shared._3rdParties.Mailgun;
+using Microsoft.AspNetCore.Http;
 using Moq;
 using UnitTesting.Common;
 using Xunit;
@@ -18,18 +19,29 @@ namespace AncillaryInfrastructure.UnitTests.Api._3rdParties;
 [Trait("Category", "Unit")]
 public class MailgunApiSpec
 {
-    private readonly Mock<IAncillaryApplication> _ancillaryApplication;
     private readonly MailgunApi _api;
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessor;
+    private readonly Mock<IMailgunApplication> _mailgunApplication;
     private readonly MailgunSignature _signature;
 
     public MailgunApiSpec()
     {
+        var recorder = new Mock<IRecorder>();
+        _httpContextAccessor = new Mock<IHttpContextAccessor>();
+        _httpContextAccessor.Setup(hca => hca.HttpContext)
+            .Returns(new DefaultHttpContext
+            {
+                Request = { IsHttps = true }
+            });
+        var caller = new Mock<ICallerContext>();
+        caller.Setup(cc => cc.CallId).Returns("acallid");
         var callerFactory = new Mock<ICallerContextFactory>();
         callerFactory.Setup(cf => cf.Create())
-            .Returns(Mock.Of<ICallerContext>());
-        _ancillaryApplication = new Mock<IAncillaryApplication>();
+            .Returns(caller.Object);
+        _mailgunApplication = new Mock<IMailgunApplication>();
         var settings = new Mock<IConfigurationSettings>();
-        settings.Setup(s => s.Platform.GetString(MailgunConstants.WebhookSigningKeySettingName, It.IsAny<string>()))
+        settings.Setup(s =>
+                s.Platform.GetString(MailgunClient.Constants.WebhookSigningKeySettingName, It.IsAny<string>()))
             .Returns("asecret");
         _signature = new MailgunSignature
         {
@@ -38,114 +50,79 @@ public class MailgunApiSpec
             Signature = "bf106940253fa7477ba4b55a027126b70037ce9b00e67aa3bf4f5bab2775d3e1"
         };
 
-        _api = new MailgunApi(callerFactory.Object, _ancillaryApplication.Object, settings.Object);
+        _api = new MailgunApi(recorder.Object, _httpContextAccessor.Object, callerFactory.Object,
+            settings.Object, _mailgunApplication.Object);
     }
 
     [Fact]
-    public async Task WhenNotifyMailgunEventAndInvalidSignature_ThenReturnsError()
+    public async Task WhenNotifyWebhookEventAndNotHttps_ThenReturnsError()
     {
-        var result = await _api.NotifyEmailDeliveryReceipt(new MailgunNotifyWebhookEventRequest
+        _httpContextAccessor.Setup(hca => hca.HttpContext)
+            .Returns(new DefaultHttpContext
+            {
+                Request = { IsHttps = false }
+            });
+
+        var result = await _api.NotifyWebhookEvent(new MailgunNotifyWebhookEventRequest
         {
             Signature = new MailgunSignature()
         }, CancellationToken.None);
 
         result().Should().BeError(ErrorCode.NotAuthenticated);
-        _ancillaryApplication.Verify(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-            It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>()), Times.Never);
+        _mailgunApplication.Verify(app => app.NotifyWebhookEvent(It.IsAny<ICallerContext>(),
+            It.IsAny<MailgunEventData>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
     [Fact]
-    public async Task WhenNotifyMailgunEventAndUnhandledEvent_ThenReturnsEmptyResponse()
+    public async Task WhenNotifyWebhookEventAndInvalidSignature_ThenReturnsError()
     {
-        var result = await _api.NotifyEmailDeliveryReceipt(new MailgunNotifyWebhookEventRequest
+        var result = await _api.NotifyWebhookEvent(new MailgunNotifyWebhookEventRequest
         {
-            Signature = _signature,
-            EventData = new MailgunEventData
-            {
-                Event = "anunknownevent"
-            }
+            Signature = new MailgunSignature()
         }, CancellationToken.None);
 
-        result().Value.Should().BeOfType<EmptyResponse>();
-        _ancillaryApplication.Verify(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-            It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>()), Times.Never);
+        result().Should().BeError(ErrorCode.NotAuthenticated);
+        _mailgunApplication.Verify(app => app.NotifyWebhookEvent(It.IsAny<ICallerContext>(),
+            It.IsAny<MailgunEventData>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+
     [Fact]
-    public async Task WhenNotifyMailgunEventAndWithNoReceiptId_ThenReturnsEmptyResponse()
+    public async Task WhenNotifyWebhookEventAndWithNEvent_ThenReturnsEmptyResponse()
     {
-        var result = await _api.NotifyEmailDeliveryReceipt(new MailgunNotifyWebhookEventRequest
+        var result = await _api.NotifyWebhookEvent(new MailgunNotifyWebhookEventRequest
         {
             Signature = _signature,
             EventData = new MailgunEventData
             {
-                Event = MailgunConstants.Events.Delivered,
                 Message = new MailgunMessage()
             }
         }, CancellationToken.None);
 
         result().Value.Should().BeOfType<EmptyResponse>();
-        _ancillaryApplication.Verify(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-            It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>()), Times.Never);
+        _mailgunApplication.Verify(app => app.NotifyWebhookEvent(It.IsAny<ICallerContext>(),
+            It.IsAny<MailgunEventData>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
     [Fact]
-    public async Task WhenNotifyMailgunEventAndWithDeliveredEvent_ThenReturnsEmptyResponse()
+    public async Task WhenNotifyWebhookEvent_ThenNotifies()
     {
-        _ancillaryApplication.Setup(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-                It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>()))
+        _mailgunApplication.Setup(app => app.NotifyWebhookEvent(It.IsAny<ICallerContext>(),
+                It.IsAny<MailgunEventData>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync(Result.Ok);
-        var deliveredAt = DateTime.UtcNow.ToNearestSecond().ToUnixSeconds();
-        var result = await _api.NotifyEmailDeliveryReceipt(new MailgunNotifyWebhookEventRequest
+        var eventData = new MailgunEventData
+        {
+            Event = "anevent"
+        };
+
+        var result = await _api.NotifyWebhookEvent(new MailgunNotifyWebhookEventRequest
         {
             Signature = _signature,
-            EventData = new MailgunEventData
-            {
-                Event = MailgunConstants.Events.Delivered,
-                Message = new MailgunMessage
-                {
-                    Headers = new MailgunMessageHeaders
-                    {
-                        MessageId = "areceiptid"
-                    }
-                },
-                Timestamp = deliveredAt
-            }
+            EventData = eventData
         }, CancellationToken.None);
 
         result().Value.Should().BeOfType<EmptyResponse>();
-        _ancillaryApplication.Verify(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-            "areceiptid", deliveredAt.FromUnixTimestamp(), It.IsAny<CancellationToken>()));
-    }
-
-    [Fact]
-    public async Task WhenNotifyMailgunEventAndWithFailedEvent_ThenReturnsEmptyResponse()
-    {
-        _ancillaryApplication.Setup(app => app.ConfirmEmailDeliveredAsync(It.IsAny<ICallerContext>(),
-                It.IsAny<string>(), It.IsAny<DateTime>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(Result.Ok);
-        var failedAt = DateTime.UtcNow.ToNearestSecond().ToUnixSeconds();
-        var result = await _api.NotifyEmailDeliveryReceipt(new MailgunNotifyWebhookEventRequest
-        {
-            Signature = _signature,
-            EventData = new MailgunEventData
-            {
-                Event = MailgunConstants.Events.Failed,
-                Message = new MailgunMessage
-                {
-                    Headers = new MailgunMessageHeaders
-                    {
-                        MessageId = "areceiptid"
-                    }
-                },
-                Timestamp = failedAt,
-                Severity = MailgunConstants.Values.PermanentSeverity,
-                Reason = "areason"
-            }
-        }, CancellationToken.None);
-
-        result().Value.Should().BeOfType<EmptyResponse>();
-        _ancillaryApplication.Verify(app => app.ConfirmEmailDeliveryFailedAsync(It.IsAny<ICallerContext>(),
-            "areceiptid", failedAt.FromUnixTimestamp(), "areason", It.IsAny<CancellationToken>()));
+        _mailgunApplication.Verify(app => app.NotifyWebhookEvent(It.Is<ICallerContext>(cc => cc.CallId == "acallid"),
+            eventData, It.IsAny<CancellationToken>()));
     }
 }
